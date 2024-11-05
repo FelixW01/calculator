55076a19cad4a2417a643da988951f0e52d0fbaa
Felix W <felixwillem01@yahoo.com>
2024-11-04 19:58:54 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// let divEl = document.getElementById('main-div');
+
+// for (let i = 0; i < 10; i++) {
+//     let newButton = document.createElement('button')
+//     newButton.textContent = i
+//     newButton.addEventListener('click', () => {
+//                 alert(`Button ${i} clicked!`);
+//             });
+//             divEl.appendChild(newButton);
+// }
