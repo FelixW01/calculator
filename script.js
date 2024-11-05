84f3eaf99f46997986adc4325be0d52f10e2f3b1
FelixW01 <felixwillem01@yahoo.com>
2024-11-05 18:13:47 -0500

    worked on adding javascript

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const userInput = document.querySelectorAll('[data-selection]');
+const displayEl = document.getElementById('display-p')
 
 userInput.forEach(userInput => {
     userInput.addEventListener('click', e => {
@@ -8,5 +9,21 @@ userInput.forEach(userInput => {
 })
 
 function calculateInput(clickedBtn) {
-
+    let num1 = 0
+    let num2 = 0
+    let display = 0;
+    let operators = ['(', ')', '%', '=', "+", "-", "x"];
+    
+    if (operators.includes(clickedBtn)) {
+        display += `${clickedBtn}`
+        console.log(clickedBtn)
+    } else if (clickedBtn == 'AC') {
+        display = 0
+    } else {
+        num1 = clickedBtn
+        display = num1 
+        console.log(typeof(num1), '<<<<<< Num1')
+        console.log(typeof(clickedBtn), '<<<<<< clickedBtn')
+    }
+    displayEl.textContent = display
 }
